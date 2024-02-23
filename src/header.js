f80824ac7c498b4f6bb8055cfd6f52095e70d3e4
ocebert31 <oceanebertrand339@gmail.com>
2024-02-23 12:05:20 +0100

    WIP

--- src/header.js
+++ src/header.js
@@ -1,24 +1,33 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import KasaLogo from "logo-kasa.png"
 import './header.scss'; // Importez le fichier Sass pour le style
 
-const Header = () => {
-    // Gestionnaires d'événements pour les boutons (identiques à l'exemple précédent)
 
-    return (
-        <header className="header">
-            <div className="header__left">
-                <img src="logo-kasa.png" alt="Kasa" />
-            </div>
-            <div className="header__right">
-                {/* <Link to="/">Accueil</Link>
-                <Link to="/a-propos">A propos</Link> */}
-                <button onClick="/">Accueil</button>
-                <button onClick="/a-propos">A propos</button>
-            </div>
-        </header>
-    );
-};
+function Bouton() {
+  const handleClick = () => {
+    // Rediriger vers une autre page
+    window.location.href = '/';
+  };
 
-export default Header;
+  const click = () => {
+    // Rediriger vers une autre page
+    window.location.href = '/a-propos';
+  };
+
+  return (
+    <header className="header">
+        <div className="header__left">
+            <img src="logo-kasa.png" alt="Kasa" />
+        </div>
+        <div className="header__right">
+            <button onClick={handleClick}>Accueil</button>
+            <button onClick={click}>A propos</button>
+        </div>
+    </header>
+
+  );
+}
+
+export default Bouton;
+
+
+
