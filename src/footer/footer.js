c332849c6281c1d24d0dab4145ff9fadaffc65d7
ocebert31 <oceanebertrand339@gmail.com>
2024-02-24 21:58:52 +0100

    footer terminé

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './footer.scss'; 
+import KasaFooter from "./kasa-footer.png"
+import DroitKasaFooter from "./droit-kasa-footer.png"
 
 function footer() {
   return (
     <footer className="footer">
-        
+        <div className='footer_top'>
+          <img src= {KasaFooter} alt="logo Kasa"/>
+        </div>
+        <div className='footer_bottom'>
+          <img src= {DroitKasaFooter} alt="droit de Kasa"/>
+        </div>
     </footer>
 
   );
