7dc253918a09b74ae45a76ee3e0739c1c3df0965
ocebert31 <oceanebertrand339@gmail.com>
2024-02-29 17:52:33 +0100

    style page logement terminé

--- src/pageLogement/containerInfoProprietaire.js
+++ src/pageLogement/containerInfoProprietaire.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import "./containerInfoProprietaire.scss";
+import Image from "./profile-picture.jpg";
+import Star from "./star-active.png"
 
 function containerInfoProprietaire () {
     return (
         <div>
-            <p>Alexandre Dumas</p>
+            <div className='position-profile'>
+                <p className='style-name'>Alexandre Dumas</p>
+                <img src={Image} alt="place tête" className='style-image-profile'/>
+            </div>
+            <div>
+                <img src={Star} alt="étoile" />
+                <img src={Star} alt="étoile" />
+                <img src={Star} alt="étoile" />
+                <img src={Star} alt="étoile" />
+                <img src={Star} alt="étoile" />
+            </div>
         </div>
     )
 }
