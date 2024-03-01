fefd54d144cfa300065a5325930472ce15623ac2
ocebert31 <oceanebertrand339@gmail.com>
2024-03-01 14:08:35 +0100

    recuperation des donnees

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Router from "./router";
 import Headers from "./header/header";
 import Footer from "./footer/footer";
+import FetchData from "../src/recuperation-donnee/fetch";
 
 function App() {
   return (
     <div className="App">
       <Headers/>
+      <FetchData />
       <Router/>
       <Footer/>
     </div>
---
+++ src/recuperation-donnee/fetch.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+const FetchData = () => {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/logement.json");
+            } catch {
+                console.log("il y a une erreur dans la récupération des données:");
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default FetchData;
