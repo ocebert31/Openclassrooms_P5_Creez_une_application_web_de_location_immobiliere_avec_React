8c80783769738f13fb406a891047640677961a1e
ocebert31 <oceanebertrand339@gmail.com>
2024-03-07 20:56:28 +0100

    wip

--- src/housing/ownerInfoContainer/Star/StarRating.js
+++ src/housing/ownerInfoContainer/Star/StarRating.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Star from "./star-active.png"
 import InactiveStar from "./star-inactive.png";
+import "./starRating.scss";
 
 function StarRating({ rating }) {
     // Convertir la note en nombre entier
@@ -10,9 +11,9 @@ function StarRating({ rating }) {
     const stars = [];
     for (let i = 0; i < 5; i++) {
         if (i < ratingValue) {
-            stars.push(<img key={i} src={Star} alt="étoile active" />);
+            stars.push(<img key={i} src={Star} alt="étoile active" className='style-star'/>);
         } else {
-            stars.push(<img key={i} src={InactiveStar} alt="étoile inactive" />);
+            stars.push(<img key={i} src={InactiveStar} alt="étoile inactive" className='style-star'/>);
         }
     }
 
--- src/housing/ownerInfoContainer/ownerInfoContainer.js
+++ src/housing/ownerInfoContainer/ownerInfoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InfoProprietaire from './owner/infoOwner';
 import StarRating from './Star/StarRating';
+import "./ownerInfoContainer.scss"
 
 function ContainerInfoProprietaire(props) {
     if (!props.item || !props.item.host) {
@@ -9,8 +10,12 @@ function ContainerInfoProprietaire(props) {
 
     return (
         <div className="container-info-proprietaire" key={props.item.id}>
-            <InfoProprietaire name={props.item.host.name} picture={props.item.host.picture} />
-            <StarRating rating={props.item.rating} />
+            <div className="owner">
+                <InfoProprietaire name={props.item.host.name} picture={props.item.host.picture} />
+            </div>
+            <div className="star">
+                <StarRating rating={props.item.rating} />
+            </div>
         </div>
     );
 }
